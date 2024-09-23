721420afdf577c40c55bd4498106fd1500d8219d
Ricardo André Simões <ricardo.andre.simoes@gmail.com>
2024-09-23 21:10:07 +0100

    .

--- TP0/mainAudio.py
+++ TP0/mainAudio.py
@@ -1,7 +1,17 @@
 from scipy.io import wavfile
 import sounddevice as sd
 
-[fs, data] = wavfile.read("./TP0/guitar.wav")
+[fs, data] = wavfile.read("guitar.wav")
 
 sd.play(data, fs)
 status = sd.wait()
+
+def apresentarInfo(nomeFicheiro, fs, nrBitsQuant):
+    print("Nome: " + nomeFicheiro)
+    print("Taxa de amostragem: %.1f kHz" % (fs / 1000))
+    print("Quantização: %d bits" % nrBitsQuant)
+    
+apresentarInfo("guitar.wav", fs, data.itemsize * 8)
+
+def visualizacaoGrafica(data, fs):
+    
