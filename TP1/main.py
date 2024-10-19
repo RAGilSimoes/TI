feecdf7841e0f98c9b99f04fe1cd05932b0f2f63
Ricardo André Simões <ricardo.andre.simoes@gmail.com>
2024-10-19 13:39:08 +0100

    .

--- TP1/main.py
+++ TP1/main.py
@@ -1,13 +1,33 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import huffmancodec as huffc
 
 
 #8/10 - 1,75 em 3 (0,25 não está tudo em funções  0,75 calcular o numero de ocorrencias)
 
 #Funções---------------------------------------------------------
+#Tópico 2 --------------------------------------------------------
+#função para construir um gráfico de MPG em relação a uma variável      
+def construirGraficoVariavelVsMPG(variavel,local, arrayInformacao):
+    plt.subplot(3,2,local)
+    plt.plot(arrayInformacao[:,local-1],arrayInformacao[:, 6], "*m")
+    plt.xlabel(variavel)
+    plt.ylabel("MPG")
+    plt.title("MPG Vs " + variavel)
+    plt.subplots_adjust(wspace = 0.5)
+    plt.subplots_adjust(hspace = 1)
+        
 
+#função para construir os gráficos de MPG em relação às variáveis e apresentá-los numa só figura
+def juntaGraficosVariavelVsMPG(varNames, arrayInformacao):
+    plt.figure(1)
+    for i in range(len(varNames) - 1):
+        construirGraficoVariavelVsMPG(varNames[i],i+1, arrayInformacao) 
+    plt.show()
 #-----------------------------------------------------------------
+
+#Tópico 4 e 6--------------------------------------------------------
 #função para calcular o número de ocorrências de cada variável e para criar um array dos números com ocorrências                                 
 def calcularNumeroOcorrencias(arrayInformacao, arrayBaseOcorrencias, dicionarioOcorrencias, alfabetoValores, varNames):
     for i in range(len(varNames)):
@@ -19,7 +39,7 @@ def calcularNumeroOcorrencias(arrayInformacao, arrayBaseOcorrencias, dicionarioO
 #-----------------------------------------------------------------
 
 
-#-----------------------------------------------------------------
+#Tópico 5 e 6--------------------------------------------------------
 #funcao para construir o gráfico dependendo da lista que recebe--
 def criaGraficoVariavel(arrayOcorrenciasRecebido, arrayValoresRecebido, variavel): 
     novoArrayOcorrencias = arrayOcorrenciasRecebido[arrayOcorrenciasRecebido > 0] #n02
@@ -40,29 +60,10 @@ def apresentaGraficosVariaveis(dicionarioOcorrencias, alfabetoValores, varNames)
 #-----------------------------------------------------------------
 
 
-#-----------------------------------------------------------------
-#função para construir um gráfico de MPG em relação a uma variável      
-def construirGraficoVariavelVsMPG(variavel,local, arrayInformacao):
-    plt.subplot(3,2,local)
-    plt.plot(arrayInformacao[:,local-1],arrayInformacao[:, 6], "*m")
-    plt.xlabel(variavel)
-    plt.ylabel("MPG")
-    plt.title("MPG Vs " + variavel)
-    plt.subplots_adjust(wspace = 0.5)
-    plt.subplots_adjust(hspace = 1)
-        
-
-#função para construir os gráficos de MPG em relação às variáveis e apresentá-los numa só figura
-def juntaGraficosVariavelVsMPG(varNames, arrayInformacao):
-    plt.figure(1)
-    for i in range(len(varNames) - 1):
-        construirGraficoVariavelVsMPG(varNames[i],i+1, arrayInformacao) 
-    plt.show()
-#-----------------------------------------------------------------
 
 
-#-----------------------------------------------------------------
-def efetuarBinning(arrayInformacao, indiceVariavel, tamanhoIntervalo): #5 valores ou 40 valores (argumentos: quantidade de valores do intervalo, array informacao especifico, dicionarios e variaveis) #encontrar o indice correspondente ao nome
+#Tópico 6 --------------------------------------------------------
+def efetuarBinning(arrayInformacao, indiceVariavel, tamanhoIntervalo): 
     array = arrayInformacao[:, indiceVariavel]
     maiorNumero = array[np.argmax(array)]
     quantidadeBlocos = (maiorNumero // tamanhoIntervalo)
@@ -91,6 +92,81 @@ def binningPrincipal(arrayInformacao, varNames, variaveisEscolhidas):
         efetuarBinning(arrayInformacao, indice, tamanhoIntervalo)
 #-----------------------------------------------------------------
 
+
+#Tópico 7 --------------------------------------------------------
+def valorMedioBits(variavelEscolhida):
+    valoresUnicos, contagem = np.unique(variavelEscolhida, return_counts=True) #conta a frequencia de cada simbolo (numero) na coluna do excel
+    probabilidades = contagem / len(variavelEscolhida) #calcula probabilidades de cada simbolo e devolve um array em que cada indice é a probabilidade de cada simbolo
+    entropia = -np.sum(probabilidades * np.log2(probabilidades)) #fórmula de Shannon
+    return entropia
+
+
+def entropia(varNames, arrayInformacao):
+    dicionarioEntropias = {}
+    print("Entropia:")
+    for i, variavel in enumerate(varNames):
+        dicionarioEntropias[variavel] = valorMedioBits(arrayInformacao[:, i])
+        print(f"{variavel}: {dicionarioEntropias[variavel]}")
+    entropia_total = valorMedioBits(arrayInformacao.flatten()) #Entropia total      #flatten tranforma multiplos arrays em 1 unico
+    print(f"Total: {entropia_total}\n")
+#-----------------------------------------------------------------
+
+
+#Tópico 8 --------------------------------------------------------
+# def obterInformacoesHuffman(source):
+#     codec = huffc.HuffmanCodec.from_data(source)
+#     symbols, lengths = codec.get_code_len() #symbols - o mesmo que o unique / length - tamanho de bits para representar cada simbolo
+#     return lengths
+
+# def valorMedioBitsHuffman(arrayInformacao):
+#     lengths = obterInformacoesHuffman(arrayInformacao)
+#     valoresUnicos, contagem = np.unique(arrayInformacao, return_counts=True) #conta a frequencia de cada simbolo (numero) na coluna do excel
+#     valorMedioBits = np.average(lengths, weights=(contagem/len(arrayInformacao))) #calcula o tamanho medio de bits por simbolo
+#     return valorMedioBits, lengths, contagem
+
+# def varianciaPonderada(arrayInformacao):
+#     valorMedioBits, lengths, contagem = valorMedioBitsHuffman(arrayInformacao)
+#     variancia = ((lengths - valorMedioBits) ** 2)
+#     varianciaPonderada = np.average(variancia, weights=(contagem/len(arrayInformacao)))
+#     return varianciaPonderada
+
+def entropiaHuffmanEVarianciaPonderadaHuffmanCadaVariavel(arrayInformacaoPassado):
+    codec = huffc.HuffmanCodec.from_data(arrayInformacaoPassado)
+    symbols, lengths = codec.get_code_len() #symbols - o mesmo que o unique / length - tamanho de bits para representar cada simbolo
+
+    valoresUnicos, contagem = np.unique(arrayInformacaoPassado, return_counts=True) #conta a frequencia de cada simbolo (numero) na coluna do excel
+    probabilidades = (contagem/len(arrayInformacaoPassado))
+    valorMedioBits = np.average(lengths, weights=probabilidades) #calcula o tamanho medio de bits por simbolo
+
+    variancia = ((lengths - valorMedioBits) ** 2)
+    varianciaPonderada = np.average(variancia, weights=probabilidades)
+    return valorMedioBits, varianciaPonderada
+
+
+def entropiaHuffmanEVarianciaPonderadaHuffman(varNames, arrayInformacao):
+    dicionarioVarianciasPonderadasHuffman = {}
+    dicionarioEntropiasHuffman = {}
+    for i, variavel in enumerate(varNames):
+        dicionarioEntropiasHuffman[variavel], dicionarioVarianciasPonderadasHuffman[variavel] = entropiaHuffmanEVarianciaPonderadaHuffmanCadaVariavel(arrayInformacao[:, i])
+    entropiaTotalHuffman, varianciaPonderadaTotalHuffman = entropiaHuffmanEVarianciaPonderadaHuffmanCadaVariavel(arrayInformacao.flatten()) #Entropia total      #flatten tranforma multiplos arrays em 1 unico
+    return dicionarioVarianciasPonderadasHuffman, dicionarioEntropiasHuffman, entropiaTotalHuffman, varianciaPonderadaTotalHuffman
+    
+    
+def apresentarEntropiaHuffmanEVarianciaPonderada(varNames, arrayInformacao):
+    dicionarioVarianciasPonderadasHuffman, dicionarioEntropiasHuffman, entropiaTotalHuffman, varianciaPonderadaTotalHuffman = entropiaHuffmanEVarianciaPonderadaHuffman(varNames, arrayInformacao)
+    print("Comprimento médio:")
+    for variavel in varNames:
+        print(f"{variavel} (por Huffman): {dicionarioEntropiasHuffman[variavel]}")
+    print(f"Total (por Huffman): {entropiaTotalHuffman}\n")
+    
+    print("Variância dos comprimentos:")
+    for variavel in varNames:
+        print(f"{variavel} (por Huffman): {dicionarioVarianciasPonderadasHuffman[variavel]}")
+    print(f"Total (por Huffman): {varianciaPonderadaTotalHuffman}\n")
+#-----------------------------------------------------------------
+
+
+
 #-----------------------------------------------------------------
 def main():
     #Tópico 1 --------------------------------------------------------
@@ -104,11 +180,13 @@ def main():
     #construir lista com os nomes das variáveis--
     varNames = data.columns.values.tolist()
     print(varNames)
+    #-----------------------------------------------------------------
     
     
     #Tópico 2 --------------------------------------------------------
     #construir os gráficos das variávieis--
     juntaGraficosVariavelVsMPG(varNames, arrayInformacao)
+    #-----------------------------------------------------------------
 
 
     #Tópico 3 --------------------------------------------------------
@@ -117,6 +195,7 @@ def main():
     #definir respetivo alfabeto
     nb = (arrayInformacao.itemsize * 8)
     arrayBaseOcorrencias = np.arange(0, 2**nb)
+    #-----------------------------------------------------------------
 
     
     #Tópico 4 --------------------------------------------------------
@@ -124,9 +203,12 @@ def main():
     #calcular o número de ocorrências e os valores em que existem ocorrencias
     dicionarioOcorrencias = {}
     calcularNumeroOcorrencias(arrayInformacao, arrayBaseOcorrencias, dicionarioOcorrencias, alfabetoValores, varNames)
+    #-----------------------------------------------------------------
+    
     
     #Tópico 5 --------------------------------------------------------
     apresentaGraficosVariaveis(dicionarioOcorrencias, alfabetoValores, varNames)
+    #-----------------------------------------------------------------
 
 
     #Tópico 6 --------------------------------------------------------
@@ -134,16 +216,26 @@ def main():
     calcularNumeroOcorrencias(arrayInformacao, arrayBaseOcorrencias, dicionarioOcorrencias, alfabetoValores, varNames)  
     apresentaGraficosVariaveis(dicionarioOcorrencias, alfabetoValores, varNames)
     #-----------------------------------------------------------------
+    
+    
+    #Tópico 7 --------------------------------------------------------
+    entropia(varNames, arrayInformacao)
 
-#topico 7
-#calcular a entropia ponto a
-#ponto b - entropia conjunta da matriz de todas as variáveis - entropia dos simbolos mas de todas as variaveis - considerar apos variadas transformadas
+    #Comentar o resultado: 
+        #Acelaracao tem um valor de entropia relativamente alto ou seja ainda existem bastantes valores variados no dataset.
+        #Cylindros tem um valor bastante baixo de entropia ou seja sao valores bastantes fixos como (4,6,8) e nao estao longe uns dos outros.
+        #Displacement tem um valor de entropia elevado ou seja tem valores bem diversificados.
+        #HorsePower tem um valor de entropia elevado mostrando uma grande variedade de elementos no dataset.
+        #ModYear tem um valor retalivamente alto mesmo tendo alguns valores fixos.
+        #Weight tem um valor de entropia mais alto de todas as variaveis tendo os valores mais distantes.
+        #Mpg tem um valor de entropia alto tendo valores bastante variados no dataset.
+        
+        #OU apenas apresentar os resultados obtidos???
+    #-----------------------------------------------------------------
 
 
-#topico 8
-#variância ponderada - np.var() calcula a variância simples
-#np.average(x) - média simples de x -- np.average(x, weigth=p) media ponderada
-#var(x) = E((x-media)^2)
+    #Tópico 8 --------------------------------------------------------
+    apresentarEntropiaHuffmanEVarianciaPonderada(varNames, arrayInformacao)
 
 
 if __name__ == "__main__":
